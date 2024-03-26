45c372424ddb874b6343b43b879a849cd7e1ffa1
Yao <yao_yu.wu@futuregames.nu>
2024-03-26 17:37:42 +0100

    save and load functions
    - added save function and load function

--- src/js/main.ts
+++ src/js/main.ts
@@ -12,3 +12,25 @@ interface Course {
     syllabus: string;
 }
 
+// function save course info 
+function saveCourse(courses: Course[]): void {
+    localStorage.setItem('courses', JSON.stringify(courses));
+    courses.forEach(course => console.log('saved course:', course.code, course.name, course.progression, course.syllabus));
+}
+
+// function load course info
+function loadCourses(): Course[] {
+    const courseJSON = localStorage.getItem('courses');
+    return courseJSON ? JSON.parse(courseJSON) : [];
+}
+
+
+
+// const courses: Course[] = [
+//     { code: '1234', name: 'Web', progression: 'A', syllabus: 'svt.se' }
+// ];
+
+// const loadedCourses = loadCourses();
+// console.log('Loaded courses:', loadedCourses);
+
+
